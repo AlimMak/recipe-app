b413744d26727e596b0bf5ec52269a5ef74ac945
AlimMak <alimmak04@gmail.com>
2025-01-02 22:12:01 -0600

    app completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const DesktopSidebar = () => {
     return (
         <div className='p3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
             <div className='flex flex-col gap-20 sticky top-10 left-0'>
-                <div classname='w-full'>
+                <div className='w-full'>
                     <img src="/logo.svg" alt="logo" className='hidden md:block' />
                     <img src="/mobile-logo.svg" alt="logo" className='block md:hidden' />
                 </div>
